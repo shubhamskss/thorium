a16a9516b0ef8d44110fb19f77448743e283aa7d
shubhamskss <shubhamkumarsingh5455@gmail.com>
2022-02-22 22:23:48 +0530

    get api

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const res = require('express/lib/response');
 const router = express.Router();
 
 router.get('/students/:name', function(req, res) {
@@ -7,7 +8,38 @@ router.get('/students/:name', function(req, res) {
     res.send(studentName)
 })
 
+router.get('/movies', function(req,res){
+    const array = ['the shining','incendies','rang de basanti','finding demo']
+    res.send(array)
+})
+router.get('/movies/:movieId', function(req,res){
+    let movies = ['the shining','incendies','rang de basanti','finding demo']
+let value = req.params.movieId
+if(value>movies.length-1){
+    res.send('movies with this id not exist')
+}
+else {res.send(movies[value])}
+})
+router.get('/films', function(req,res){
+    res.send([{"id":1,"name":"incendies"},
+    {"id":2,"name":"incendies"},
+    {"id":3,"name":"rang de basanti"},
+    {"id":4,"name":"finding demo"} ]       )
+})
+router.get('/films/:filmid',function(req,res){
+    let arr = [{"id":1,"name":"incendies"},
+    {"id":2,"name":"incendies"},
+    {"id":3,"name":"rang de basanti"},
+    {"id":4,"name":"finding demo"} ]
+    let value = req.params.filmid
+    for(i=0;i<arr.length;i++){
+        if(arr[i].id==value){
+            res.send(arr[i])
+            break;
+        }
+        else{res.send("id not found")}
+    }
 
 
-
+})
 module.exports = router;
