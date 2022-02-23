78958a68fd9e59b9fea8d6f7edeafbfa1399d62f
shubhamskss <shubhamkumarsingh5455@gmail.com>
2022-02-23 23:31:39 +0530

    assignmenttried

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,73 +1,172 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/students/:name', function(req, res) {
-    let studentName = req.params.name
-    console.log(studentName)
-    res.send(studentName)
-})
+  
+   
+  
 
-router.get("/random" , function(req, res) {
-    res.send("hi there")
-})
+  
+  let player = [{
+  
+    "name": "manish",
+  
+    "dob": "1/1/1995",
+  
+    "gender": "male",
+  
+    "city": "jalandhar",
+  
+    "sports": [
+  
+      "swimming"
+  
+    ],
+  
+    "bookings": [
+  
+      
+  
+  {
+  
+   "bookingnumber":1,
+  
+    "sportsid":"ab",
+  
+    "centerid":"cd",
+  
+   "type":"private",
+  
+   "slot": "162865980",
+  
+   "bookedOn": "31/08/2021",
+  "bookedfor": "1/09/2021"
+   
+  
+  },]},
+  
+   
+  
+  
+  
+   
+  
+
+  
+  
+  {
+  
+    "name": "gopal",
+  
+    "dob": "1/09/1995",
+  
+    "gender": "male",
+  
+    "city": "delhi",
+  
+    "sports": [
+  
+      "soccer"
+  
+    ],
+  
+    "bookings": [{
+        "bookingnumber":2,
+       
+         "sportsid":"ef",
+       
+         "centerid":"gh",
+       
+        "type":"private",
+       
+        "slot": "162865981",
+       
+        "bookedOn": "2/09/2021",
+       "bookedfor": "3/09/2021"}
+        
+  
+      
+  
+    ]
+  
+  },
+  
+  {
+  
+    "name": "lokesh",
+  
+    "dob": "1/1/1990",
+  
+    "gender": "male",
+  
+    "city": "mumbai",
+  
+    "sports": [
+  
+      "soccer"
+  
+    ],
+  
+    "bookings": [
+  
+       
+      
+  
+    ],
+  
+  },
+  
+  
+  
+  ]
+  
+   router.post('/playerss', function(req,res){
+       let newplayer=req.body.newplayer
+       for(i=0;i<player.length;i++){
+           if(player[i].name==newplayer.name){
+               res.send({"msg":"player already exist"})
+           }
+           
+       else{
+       player.push(newplayer)
+       res.send({"msg":player})}}
+   })
+   router.post('/playersss/:playername', function(req,res){
+let value = req.params.playername
 
 
-router.get("/test-api" , function(req, res) {
-    res.send("hi FunctionUp")
-})
-
-
-router.get("/test-api-2" , function(req, res) {
-    res.send("hi FunctionUp. This is another cool API")
-})
-
-
-router.get("/test-api-3" , function(req, res) {
-    res.send("hi FunctionUp. This is another cool API. And NOw i am bored of creating API's ")
-})
-
-
-router.get("/test-api-4" , function(req, res) {
-    res.send("hi FunctionUp. This is another cool API. And NOw i am bored of creating API's. PLZ STOP CREATING MORE API;s ")
-})
+let resultobj =req.body.resultobj
+for(let i=0;i<player.length;i++){
+    if(player[i].name!=value){
+        res.send({"msg":"Player not exist"})
+    }
+    if(player[i].name==value && player[i].bookings=='[]'){
+        player.push(resultobj)
+        res.send({"msg":"resultobj"})
+    }
+    else{res.send({"msg":"player id already exist"})}
+}
 
 
 
-router.get("/test-api-5" , function(req, res) {
-    res.send("hi FunctionUp5. This is another cool API. And NOw i am bored of creating API's. PLZ STOP CREATING MORE API;s ")
-})
-
-router.get("/test-api-6" , function(req, res) {
-    res.send({a:56, b: 45})
-})
-
-router.post("/test-post", function(req, res) {
-    res.send([ 23, 45 , 6])
-})
-
-
-router.post("/test-post-2", function(req, res) {
-    res.send(  { msg: "hi" , status: true }  )
-})
-
-router.post("/test-post-3", function(req, res) {
-    // let id = req.body.user
-    // let pwd= req.body.password
-
-    // console.log( id , pwd)
-
-    console.log( req.body )
-
-    res.send(  { msg: "hi" , status: true }  )
-})
+   })
+   
 
 
 
-router.post("/test-post-4", function(req, res) {
-    let arr= [ 12, "functionup"]
-    let ele= req.body.element
-    arr.push(ele)
-    res.send(  { msg: arr , status: true }  )
-})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 module.exports = router;
