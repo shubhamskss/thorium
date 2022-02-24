05cd5cf244c38047e8dfb1191acdf09add9fa431
shubhamskss <shubhamkumarsingh5455@gmail.com>
2022-02-25 00:50:49 +0530

    updated assignment

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,172 +1,45 @@
 const express = require('express');
 const router = express.Router();
 
-  
-   
-  
-
-  
-  let player = [{
-  
-    "name": "manish",
-  
-    "dob": "1/1/1995",
-  
-    "gender": "male",
-  
-    "city": "jalandhar",
-  
-    "sports": [
-  
-      "swimming"
-  
-    ],
-  
-    "bookings": [
-  
+  let player=[]
+  router.post('/players', function(req,res){
+    let playerss=req.body
+    for(let i=0;i<player.length;i++){
+      if(player[i].name==playerss.name){
+       return res.send({"response":"player already exist"})
+      }}
       
+      player.push(playerss)
+      res.send({player})
+    
+  })
+   router.post('/player1/:playername/bookings/:bookingid', function(req,res){
+ let playerpresent=false
+ let name=req.params.playername
+console.log(player)
+ for(let i=0;i<player.length;i++){
+   if(player[i].name==name){
+     playerpresent=true
+   }
+ }
+ if(!playerpresent){
+   return res.send("player not present")
+ }
+ let booking=req.body
+ let bookingid=req.params.bookingid
+ for(let i=0;i<player.length;i++){if(player[i].name==name){
+ for(let j=0;j<player[i].bookings.length;j++){
+   if(player[i].bookings[j].bookingNumber==bookingid){
+     return res.send("booking with this id is already present")
+   }
+ }
+player[i].bookings.push(booking)}}
+res.send(player)
   
-  {
-  
-   "bookingnumber":1,
-  
-    "sportsid":"ab",
-  
-    "centerid":"cd",
-  
-   "type":"private",
-  
-   "slot": "162865980",
-  
-   "bookedOn": "31/08/2021",
-  "bookedfor": "1/09/2021"
-   
-  
-  },]},
-  
-   
-  
-  
-  
-   
-  
-
-  
-  
-  {
-  
-    "name": "gopal",
-  
-    "dob": "1/09/1995",
-  
-    "gender": "male",
-  
-    "city": "delhi",
-  
-    "sports": [
-  
-      "soccer"
-  
-    ],
-  
-    "bookings": [{
-        "bookingnumber":2,
-       
-         "sportsid":"ef",
-       
-         "centerid":"gh",
-       
-        "type":"private",
-       
-        "slot": "162865981",
-       
-        "bookedOn": "2/09/2021",
-       "bookedfor": "3/09/2021"}
-        
-  
-      
-  
-    ]
-  
-  },
-  
-  {
-  
-    "name": "lokesh",
-  
-    "dob": "1/1/1990",
-  
-    "gender": "male",
-  
-    "city": "mumbai",
-  
-    "sports": [
-  
-      "soccer"
-  
-    ],
-  
-    "bookings": [
-  
-       
-      
-  
-    ],
-  
-  },
-  
-  
-  
-  ]
-  
-   router.post('/playerss', function(req,res){
-       let newplayer=req.body.newplayer
-       for(i=0;i<player.length;i++){
-           if(player[i].name==newplayer.name){
-               res.send({"msg":"player already exist"})
-           }
-           
-       else{
-       player.push(newplayer)
-       res.send({"msg":player})}}
-   })
-   router.post('/playersss/:playername', function(req,res){
-let value = req.params.playername
-
-
-let resultobj =req.body.resultobj
-for(let i=0;i<player.length;i++){
-    if(player[i].name!=value){
-        res.send({"msg":"Player not exist"})
-    }
-    if(player[i].name==value && player[i].bookings=='[]'){
-        player.push(resultobj)
-        res.send({"msg":"resultobj"})
-    }
-    else{res.send({"msg":"player id already exist"})}
-}
 
 
 
    })
-   
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 module.exports = router;
