573299af933eeeaaa638ca04588a0ab0395fea56
shubhamskss <shubhamkumarsingh5455@gmail.com>
2022-03-10 22:55:54 +0530

    tryCatch_http status codes

--- src/controllers/user8controller.js
+++ src/controllers/user8controller.js
@@ -1,62 +1,82 @@
 let user8model=require("../models/user8model")
 let jwt=require('jsonwebtoken')
 let createuser=async function(req,res){
+try{
     let data=req.body
+    if(Object.keys(data).length==0){res.status(400).send({msg:"Bad request"})}
     let savedata=await user8model.create(data)
-    res.send(savedata)
+    res.status(201).send(savedata) }
+    catch(err){res.status(500).send({msg:"error",err:err.message})}
 }
 let loginuser=async function(req,res){
-    let username=req.body.emailId
+   
+   try{ let username=req.body.emailId
+if(!username){res.status(400).send({msg:"Bad request"})}
     let password=req.body.password
+    if(!password){res.status(400).send({msg:"Bad request"})}
     let savelogin=await user8model.findOne({emailId:username,password:password})
-    if(!savelogin){return res.send({msg:"username or paasword is not correct"})}
+    if(!savelogin){return res.status(400).send({msg:"username or paasword is not correct"})}
 let token=await jwt.sign({userId:savelogin._id.toString()},'shubham kumar')
 res.setHeader("x-auth-token", token)
-res.send({status:true,data:token})
-
+res.status(201).send({status:true,data:token})}
+catch(err){res.status(500).send({msg:"error",err:err.message})}
 
 }
 let getuser=async function(req,res){
-    let token=req.headers["x-auth-token"]
-   if(!token){return res.send({status:false,msg:"token required"})}
+//     let token=req.headers["x-auth-token"]
+//    if(!token){return res.send({status:false,msg:"token required"})}
 
-    let decodetoken=jwt.verify(token,'shubham kumar')
- if(!decodetoken){res.send("invalid token")}
-    let usertobeModified=req.params.userId
-    let userloggedin=decodetoken.userId
-    if(usertobeModified!=userloggedin){return res.send({status:false,msg:"not authorised"})}
-    let userId=req.params.userId
+//     let decodetoken=jwt.verify(token,'shubham kumar')
+//  if(!decodetoken){res.send("invalid token")}
+//     let usertobeModified=req.params.userId
+//     let userloggedin=decodetoken.userId
+    // if(usertobeModified!=userloggedin){return res.send({status:false,msg:"not authorised"})}
+  try{  let userId=req.params.userId
+    if(!userId){res.status(400).send({msg:"Bad request"})}
     let userDetails=await user8model.findById(userId)
-    res.send({satus:true,data:userDetails})
+    if(!userDetails){res.status(400).send({msg:"Bad request"})}
+    res.status(201).send({satus:true,data:userDetails})}
+    catch(err){res.status(500).send({msg:"error",err:err.message})}
 
 }
 let updateuser=async function(req,res){
-     let token=req.headers["x-auth-token"]
-     if(!token){return res.send({status:false,msg:"token required"})}
+//      let token=req.headers["x-auth-token"]
+//      if(!token){return res.send({status:false,msg:"token required"})}
     
-    let decodetoken=jwt.verify(token,'shubham kumar')
- if(!decodetoken){res.send("invalid token")}
- 
+//     let decodetoken=jwt.verify(token,'shubham kumar')
+//  if(!decodetoken){res.send("invalid token")}
+ try{
     const userId=req.params.userId
+    console.log(userId)
+    if(!userId){res.status(400).send({msg:"Bad request"})}
     let message=req.body.message
+    if(!message){res.status(400).send({msg:"Bad request"})}
     let user=await user8model.findById(userId)
-    if(!user){return res.send({status:false,msg:"user not found"})}
-    let usertobemodified=req.params.userId
- let userloggedin=decodetoken.userId
- if(usertobemodified!=userloggedin){return res.send({status:false,msg:"not aurhorised"})}
+    console.log(user)
+    if(!user){return res.status(404).send({status:false,msg:"user not found"})}
+//     let usertobemodified=req.params.userId
+//  let userloggedin=decodetoken.userId
+//  if(usertobemodified!=userloggedin){return res.send({status:false,msg:"not aurhorised"})}
   let  updatedposts=user.posts
     updatedposts.push(message)
-    let updateuser=await user8model.findOneAndUpdate({_id:userId},{posts:updatedposts},{new:true})//we dont take userdata in curlybracket since data come from body in json form
-res.send({status:updateuser,data:updateuser})
-}
+    let updateuser=await user8model.findOneAndUpdate({_id:userId},{posts:updatedposts},{new:true})
+res.status(201).send({status:updateuser,data:updateuser})}
+catch(err){res.status(500).send({msg:"error",err:err.message})}}
+
 const deleteUser = async function(req,res){
     // let token = req.headers["x-auth-token"];
     // if (!token) return res.send({ status: false, msg: "token must be present" });
-    
+    // let decodetoken=jwt.verify(token,'shubham kumar')
+    // if(!decodetoken){res.send("invalid token")}
+    // let usertobemodified=req.params.userId
+    // let userloggedin=decodetoken.userId
+    // if(usertobemodified!=userloggedin){return res.send({status:false,msg:"not aurhorised"})}
+try{
     let userId = req.params.userId;
-    
+    if(!userId){res.status(400).send({msg:"Bad request"})}
     let userDelete = await user8model.findOneAndUpdate({_id:userId},{isDeleted:true},{new:true});
-    res.send({status:true, data:userDelete})
+    res.status(201).send({status:true, data:userDelete})}
+    catch(err){res.status(500).send({msg:"error",err:err.message})}
   };
 
 module.exports.createuser=createuser
--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,24 +1,26 @@
 let jwt=require('jsonwebtoken')
 let mid1=function(req,res,next){
 
-
+try{
 
 let token=req.headers["x-auth-token"]
-    if(!token){return res.send({status:false,msg:"token required"})}
+    if(!token){return res.status(400).send({status:false,msg:"token required"})}
 
     let decodetoken=jwt.verify(token,'shubham kumar')
-    if(!decodetoken){res.send("invalid token")}
-next()
+    if(!decodetoken){res.status(401).send("invalid token")}
+next()}
+catch(err){res.status(500).send({mmsg:"error",err:err.message})}
 }
 let authorisation=function(req,res,next){
-    let token=req.headers["x-auth-token"]
-    if(!token){return res.send({status:false,msg:"token required"})}
+  try{  let token=req.headers["x-auth-token"]
+    if(!token){return res.status(400).send({status:false,msg:"token required"})}
     let decodetoken=jwt.verify(token,'shubham kumar')
-    if(!decodetoken){res.send("invalid token")}
+    if(!decodetoken){res.status(401).send("invalid token")}
     let usertobemodified=req.params.userId
     let userloggedin=decodetoken.userId
-    if(usertobemodified!=userloggedin){return res.send({status:false,msg:"not aurhorised"})}
-    next()
-}
+    if(usertobemodified!=userloggedin){return res.status(403).send({status:false,msg:"not aurhorised"})}
+    next()}
+    catch(err){res.send({msg:"error",err:err.message})
+}}
 module.exports.mid1=mid1
 module.exports.authorisation=authorisation
