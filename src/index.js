4d12aca3ad48cf4a1bb771b95dca0c3c1550b739
Pritesh Kumar <pritesh@gmail.com>
2022-04-01 19:44:20 +0530

    s3 and promises

--- src/index.js
+++ src/index.js
@@ -3,10 +3,11 @@ const bodyParser = require('body-parser');
 const route = require('./routes/route.js');
 const { default: mongoose } = require('mongoose');
 const app = express();
+const multer = require("multer")
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
+app.use(multer().any())
 
 mongoose.connect("mongodb+srv://functionup-cohort:G0Loxqc9wFEGyEeJ@cluster0.rzotr.mongodb.net/Pritesh8769811-DB?retryWrites=true&w=majority", {
     useNewUrlParser: true
@@ -14,12 +15,6 @@ mongoose.connect("mongodb+srv://functionup-cohort:G0Loxqc9wFEGyEeJ@cluster0.rzot
 .then( () => console.log("MongoDb is connected"))
 .catch ( err => console.log(err) )
 
-// app.use (
-//     function (req, res, next) {
-//         console.log ("inside GLOBAL MW");
-//         next();
-//   }
-//   );
 
 app.use('/', route);
 
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,7 +1,7 @@
 const express = require('express');
+const removeUploadedFiles = require('multer/lib/remove-uploaded-files');
 const router = express.Router();
-const CowinController= require("../controllers/cowinController")
-
+const aws = require("aws-sdk")
 
 
 router.get("/test-me", function (req, res) {
@@ -9,14 +9,74 @@ router.get("/test-me", function (req, res) {
 })
 
 
-router.get("/cowin/states", CowinController.getStates)
-router.get("/cowin/districtsInState/:stateId", CowinController.getDistricts)
-router.get("/cowin/getByPin", CowinController.getByPin)
 
-router.post("/cowin/getOtp", CowinController.getOtp)
+// S3 and cloud storage 
+// step 1: multer will be used as usual to get access to the file in nodejs( from previous session learnings)
+// step 2(BEST PRACTICE): always write s2 uploadFile function seperately- in a seperate file/function..expect it to take file as input and return the url of the uploaded file as output
+// step 3: aws-sdk install - as package
+// step 4: Setup config for aws authentication- use code below as plugin keys that are given to you
+// step 5: build the uploadFile function for uploading file - use the code below and edit what is marked HERE only
 
-// WRITE A GET API TO GET THE LIST OF ALL THE "vaccination sessions by district id" for any given district id and for any given date
+// PROMISES:-
+// - you can never use await on callback..if you have awaited something , then you can be sure that it is inside a promise
+// - how to write promise:- wrap your entire code inside: "return new Promise( function(resolve, reject) { "...and when error - return reject( err )..else when all ok and you have data, return resolve (data)
 
+aws.config.update(
+    {
+        accessKeyId: "AKIAY3L35MCRVFM24Q7U",
+        secretAccessKeyId: "qGG1HE0qRixcW1T1Wg1bv+08tQrIkFVyDFqSft4J",
+        region: "ap-south-1"
+    }
+)
 
+let uploadFile = async (file) => {
+    return new Promise( function(resolve, reject) {
+        //this function will upload file to aws and return the link
+        let s3 = new aws.S3({ apiVersion: "2006-03-01" }) //we will be using s3 service of aws
+         await uploadFile(files[0])
+        var uploadParams = {
+            ACL: "public-read",
+            Bucket: "classroom-training-bucket", // HERE
+            Key: "radhika/" + file.originalname, // HERE "radhika/smiley.jpg"
+            Body: file.buffer
+        }
+
+      s3.upload(uploadParams, function (err, data) {
+            if (err) { 
+                return reject({ "error": err }) 
+            }
+
+            console.log(data)
+            console.log(" file uploaded succesfully ")
+            return resolve(data.Location) // HERE
+          }
+        )
+
+    // let data= await s3.upload(uploadParams)
+    // if (data) return data.Location
+    // else return "there is an error"
+
+    }
+    )
+}
+
+router.post("/write-file-aws", async function (req, res) {
+    try {
+        let files = req.files
+        if (files && files.length > 0) {
+            //upload to s3 and get the uploaded link
+            // res.send the link back to frontend/postman
+            let uploadedFileURL = await uploadFile(files[0])
+            res.status(201).send({ msg: "file uploaded succesfully", data: uploadedFileURL })
+        }
+        else {
+            res.status(400).send({ msg: "No file found" })
+        }
+    }
+    catch (err) {
+        res.status(500).send({ msg: err })
+    }
+}
+)
 
 module.exports = router;
